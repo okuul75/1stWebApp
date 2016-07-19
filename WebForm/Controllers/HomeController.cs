537bbf25937122fed101e23498d0d08a2dc1f4cc
okuul75 <okuul@poczta.onet.pl>
2016-07-19 23:06:59 +0200

    20160719

---
+++ WebForm/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebForm.Models;
+
+namespace WebForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Powitanie =  hour < 17 ? "Dzień dobry !" : "Dobry wieczór !";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // tu ma być wysyłka zawartości guestResponse
+
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // ponowne wyświetlenie formularza
+                return View();
+            }
+
+        }
+    }
+}
---
+++ WebForm/Models/GuestResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace WebForm.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podać imię i nazwisko.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać e-mail.")]
+        [RegularExpression("^.+\\@.+\\..+$", ErrorMessage = "Proszę podać poprawny adres e-mail.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nr telefonu.")]
+        [RegularExpression("^[0-9]{9}$", ErrorMessage = "Proszę podać poprawny nr telefonu.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Proszę określić, czy weźmiesz udział.")]
+        public bool? WillAttend { get; set; }
+    }
+}
